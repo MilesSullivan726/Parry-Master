2acb66f8ea161e025d2c83160db7352f6bdf17dd
MilesSullivan726 <masullivan726@gmail.com>
2024-11-25 16:47:37 -0500

    player + enemy work
    Player character can jump, move left or right, parry, and attack. Enemy is currently stationary and attacks on a loop. Enemy can be parried and counter attacked while stunned

--- Assets/Scipts/BasicEnemy.cs
+++ Assets/Scipts/BasicEnemy.cs
@@ -7,6 +7,9 @@ public class BasicEnemy : MonoBehaviour
 
     public GameObject attackHitbox;
     private Vector3 attackPos;
+    private int hp = 3;
+    public GameObject player;
+    private bool canBeHit = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,37 @@ public class BasicEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+  
+
+        // enemy dead
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator Stunned()
+    {
+        canBeHit = true;
+        yield return new WaitForSeconds(2);
+        canBeHit = false;
     }
 
     private void Attack()
     {
         attackPos = new Vector3(transform.position.x - 1.0f, transform.position.y, transform.position.z);
-        Instantiate(attackHitbox, attackPos, transform.rotation);
+        Instantiate(attackHitbox, attackPos, transform.rotation, gameObject.transform);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player Attack") && canBeHit)
+        {
+            hp -= 1;
+            Debug.Log("Enemy HP: " + hp);
+        }
+    }
+
+ 
 }
--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -10,8 +11,15 @@ public class Player : MonoBehaviour
     public int speed;
     public int jumpHeight;
     private SpriteRenderer parryTest;
-    private bool parrySuccess = false;
-    private float parryActiveTime;
+    public bool parrySuccess = false;
+    public GameObject attackPrefab;
+    private int direction;
+    private Vector3 attackPos;
+    private int hp = 3;
+    public TextMeshProUGUI hpCounter;
+    private bool hasJumped = false;
+    private float lastParry = 0;
+    private float lastAttack = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -26,28 +34,69 @@ public class Player : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         rigidBody.AddForce(Vector2.right * horizontalInput * Time.deltaTime * speed, ForceMode2D.Impulse);
 
-        if (Input.GetKeyDown(KeyCode.K))
+        //track direction for spawning of attack hitbox
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            parryActiveTime = 0;
-            StartCoroutine(Parry());
-            Debug.Log("parry start");
-            while(parryActiveTime <= 5f)
-            {
-                parryActiveTime += Time.deltaTime;
-                Debug.Log(parryActiveTime);
-                parrySuccess = true;
-            }
-            Debug.Log("parry end");
-            parrySuccess = false;
+            direction = 0; //left
         }
 
-        /*
-        if (Input.GetKeyDown(KeyCode.Space))
+        else if (Input.GetKeyDown(KeyCode.D))
         {
+            direction = 1; //right
+        }
+
+        //parry
+        if (Input.GetKeyDown(KeyCode.K) && (Time.time - lastParry >= 1f))
+        {
+            lastParry = Time.time;
+            StartCoroutine(Parry());
+        }
+
+        //attack
+        if (Input.GetKeyDown(KeyCode.L) && Time.time - lastAttack >= 0.5f)
+        {
+            lastAttack = Time.time;
+            Attack(direction);
+        }
+
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && !hasJumped)
+        {
+            hasJumped = true;
             rigidBody.AddForce(Vector2.up * Time.deltaTime * jumpHeight, ForceMode2D.Impulse);
         }
-        */
+        
 
+        //game over
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // prevent infinite jumps
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            hasJumped = false;
+        }
+    }
+
+    private void Attack(int direction)
+    {
+        if (direction == 0) // facing left
+        {
+            attackPos = new Vector3(transform.position.x - 1.0f, transform.position.y, transform.position.z);
+            Instantiate(attackPrefab, attackPos, transform.rotation);
+        }
+
+        if (direction == 1) // facing right
+        {
+            attackPos = new Vector3(transform.position.x + 1.0f, transform.position.y, transform.position.z);
+            Instantiate(attackPrefab, attackPos, transform.rotation);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -57,10 +106,14 @@ public class Player : MonoBehaviour
             if(parrySuccess == true)
             {
                 Debug.Log("parried");
+                //stun attacking enemy so they can be damaged
+                StartCoroutine(collision.transform.parent.GetComponent<BasicEnemy>().Stunned());
             }
 
             else
             {
+                hp -= 1;
+                hpCounter.text = "HP: " + hp;
                 Debug.Log("hit");
             }
         }
@@ -68,8 +121,10 @@ public class Player : MonoBehaviour
 
     IEnumerator Parry()
     {
+        parrySuccess = true;
         parryTest.enabled = false;
         yield return new WaitForSeconds(0.5f);
         parryTest.enabled = true;
+        parrySuccess = false;
     }
 }
